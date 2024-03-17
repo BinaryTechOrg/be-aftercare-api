1482af5b86ab2a7105f352705e3e236406866e2c
momo <momo@MacBook-Pro-de-momo.local>
2024-03-17 16:07:12 -0400

    initialization of project

---
+++ src/main/java/com/aftercare/aftercareapi/AftercareApiApplication.java
@@ -0,0 +1,13 @@
+package com.aftercare.aftercareapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AftercareApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AftercareApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aftercare/aftercareapi/AftercareApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aftercare.aftercareapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AftercareApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
